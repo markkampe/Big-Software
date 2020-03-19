b1471ec7b9378ecdc3485614174b481aeecc86af
Mark.Kampe <mark.kampe@gmail.com>
2020-03-19 15:24:49 -0700

    Makefile builds single projects.html from indivdual pieces

---
+++ projects/projects.c
@@ -0,0 +1,29 @@
+#include "courseinfo.h"
+<html>
+<head>
+<title>NUMBER Projects</title>
+</head>
+<body>
+
+<center>
+<h1>
+NUMBER Projects
+#if MONTH >= 9
+(Fall YEAR)
+#else
+(Spring YEAR)
+#endif
+</h1>
+</center>
+
+#include "swe_dates.h"
+
+#include "proj_1.h"
+
+#include "proj_2.h"
+
+#include "proj_3.h"
+
+#include "proj_4.h"
+</body>
+</html>
