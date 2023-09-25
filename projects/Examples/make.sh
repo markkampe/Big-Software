f21d3fe2038e2f1c42d0f27f9b261c83679d77f6
Mark.Kampe <mark.kampe@gmail.com>
2023-09-25 13:29:46 -0700

    using python 2.7 for these secripts

--- projects/Examples/make.sh
+++ projects/Examples/make.sh
@@ -8,7 +8,7 @@ PROSE="overview.html testing.html"
 SPECS="gameobject gameaction dice gameactor gamecontext"
 SOURCES="base.py gameobject.py gameaction.py dice.py gameactor.py gamecontext.py"
 
-COURSE="cs181aa.f21"
+COURSE="cs181aa.s24"
 TARGET="markk@knuth.cs.hmc.edu:public_html"
 
 # is this a push
@@ -54,14 +54,14 @@ echo ... importing source files
 for file in $SOURCES
 do
 	echo "    $file"
-	python code_strip.py --privates --globals --comments --stop="# UNIT TEST" $SOURCE_DIR/$file $file
+	python2 code_strip.py --privates --globals --comments --stop="# UNIT TEST" $SOURCE_DIR/$file $file
 done
 
 echo ... creating simpler specification versions
 for file in $SPECS
 do
 	echo "    $file.py -> $file-spec.py"
-	python code_strip.py $file.py $file-spec.py
+	python2 code_strip.py $file.py $file-spec.py
 done
 
 echo ... creating class documentation
