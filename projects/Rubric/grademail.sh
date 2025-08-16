ac08537b794a316fe35698522f942ddfd18ababe
Mark Kampe <mark.kampe@gmail.com>
2025-08-16 09:19:36 -0700

    unification of grademail scripts

--- projects/Rubric/grademail.sh
+++ projects/Rubric/grademail.sh
@@ -3,20 +3,23 @@
 # Process per-user score files and send each, as e-mail, to the associated student.
 #
 # NOTES:
-#
-#	score files are assumed:
-#		to have names of the form <user>_<project>.txt
+#	this script correctly handles multiple comma-separated 
+#	submitters on a single EMAIL: line
 #
 #	the script tries to identify obviously bogus score files
-#	(e.g. that have no total or no known user) and will not
+#	(e.g. that have no total or email address) and will not
 #	try to send messages for them, but it is best to run the
 #	script with the "--test" option and clean up any garbage
 #	before the final run.
 #
-USAGE="$0 [--test] score-file ..."
+#	the --fixok option tells the program it is OK to send out
+#	scores that still have FIX lines in them ... which they
+#	should not.
+#
+USAGE="$0 [--test] [--fixok] score-file ..."
 
-TEAMS=teams.csv
-COURSENAME=./course_title
+NAMEFILE=./course_title
+DEBUG=0
 
 # see if we are only testing for presence of all information
 if [ -n "$1" -a "$1" == "--test" ]; then
@@ -34,26 +37,23 @@ else
 	let checkfixes=1
 fi
 
-# figure out the course name and assignment number
-if [ -s "$1" ]; then
-	ASSGT=`echo $1 | cut -d_ -f2 | cut -d. -f1`
-	if [ -s $COURSENAME ]; then
-	    course=`cat $COURSENAME`
-	    echo "Course: $course, Assignment: $ASSGT"
-	    ASSGT="$course $ASSGT"
-	else
-	    echo "Course: unspecified, Assignment: $ASSGT"
-	fi
+# figure out the course name
+if [ -s $NAMEFILE ]; then
+    COURSE=`cat $NAMEFILE`
 else
-	>&2 echo Usage: $USAGE 
-	exit 1
+    >& 2 echo "Course: unspecified, please create $NAMEFILE"
+    COURSE='CS???'
 fi
 
 let errors=0
-# sendMail address assignment scorefile total
+# sendMail address assignment scorefile comment
 function sendMail {
 	ats=`echo $1 | grep '@' | wc -l`
 	if [ $ats -eq 1 ]; then
+		# if they wanted to see the detailed command
+		if [ $DEBUG -eq 1 ]; then
+		    echo "mutt -s \"$2\" $1 < $3"
+		fi
 		echo -e -n "   $3 ($4) \t -> $1 ... " 
 		if [ $testing -eq 0 ]; then
 			mutt -s "$2" $1 < $3
@@ -75,15 +75,16 @@ function sendMail {
 # iterate through all specified files
 while [ -n "$1" ]; do
 	if [ ! -s $1 ]; then
-		echo "    $1 ... UNABLE TO OPEN"
+		>& 2 echo "    $1 ... UNABLE TO OPEN"
 		let errors+=1
 	else 
 		# some people edit their files on Windows!!!
 		tr '\r' '\n' < $1 > /tmp/$$
 
 		# pull some interesting information out of the assignment
-		EMAIL=`grep "EMAIL:" /tmp/$$ | grep -v "missing" | cut -s -d: -f2 | tr -d \[:blank:\]`
+		EMAIL=`grep -i "EMAIL:" /tmp/$$ | grep -v "missing" | cut -s -d: -f2 | tr -d \[:blank:\]`
 		TOTAL=`grep -i "total score:" /tmp/$$ | cut -s -d: -f2 | tr -d \[:blank:\]`
+		ASSGT=`grep -i "ASSIGNMENT:" /tmp/$$ | cut -s -d: -f2 | xargs`
 
 		# see if there are any FIX tags left in the file
 		if [ $checkfixes -ne 0 ]; then
@@ -113,12 +114,12 @@ while [ -n "$1" ]; do
 			if [ -n "$email" ]; then
 				field=1
 				while [ -n "$email" ]; do
-					sendMail $email "$ASSGT" $1 $TOTAL
+					sendMail $email "$COURSE: $ASSGT" $1 $TOTAL
 					let field+=1
 					email=`echo $EMAIL | cut -d, -f$field`
 				done
 			else
-				sendMail $EMAIL "$ASSGT" $1 $TOTAL
+				sendMail $EMAIL "$COURSE: $ASSGT" $1 $TOTAL
 			fi
 		fi
 
